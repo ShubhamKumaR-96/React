80303023cb3d5d874bce9780072007051377285d
ShubhamKumaR-96 <shubhamkas245@gmail.com>
2024-11-26 15:38:00 +0530

    router-dom project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,44 @@
 import React from "react";
-import BgChanger from "./components/BgChanger";
-import PasswordGenerator from "./components/PasswordGenerator";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ShoppingCart from "./components/ShoppingCart";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
+import User from "./components/Followers/User";
+import Github, { githubInfoLoader } from "./components/Followers/GithubFollowers";
+
+const router=createBrowserRouter([{
+    path:'/',
+    element:<ShoppingCart />,
+    children:[
+      {
+        path:'',
+        element:<Home />
+      },
+      {
+        path:'about',
+        element:<About />
+      },{
+        path:'contact',
+        element:<Contact />
+      },{
+        path:'/user:/userId',
+        element:<User />
+      },
+      {
+        
+        path:"github",
+        element:<Github />,
+        loader:githubInfoLoader
+      }
+    ]
+}])
 
 const App = () => {
   return (
-    <div className="flex justify-center items-center mt-20">
-      <PasswordGenerator />
-    </div>
+   <RouterProvider router={router} >
+
+   </RouterProvider>
   );
 };
 
